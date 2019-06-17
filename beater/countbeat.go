d76b413844390d99dd6641803c58067b071f43c6
HariomSaini <hariomsaini0011@gmail.com>
2019-06-17 16:28:22 +0530

    initial commit

--- beater/countbeat.go
+++ beater/countbeat.go
@@ -20,14 +20,14 @@ type Countbeat struct {
 
 // New creates an instance of countbeat.
 func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
-	c := config.DefaultConfig
-	if err := cfg.Unpack(&c); err != nil {
+	config := config.DefaultConfig
+	if err := cfg.Unpack(&config); err != nil {
 		return nil, fmt.Errorf("Error reading config file: %v", err)
 	}
 
 	bt := &Countbeat{
 		done:   make(chan struct{}),
-		config: c,
+		config: config,
 	}
 	return bt, nil
 }
@@ -36,33 +36,26 @@ func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
 func (bt *Countbeat) Run(b *beat.Beat) error {
 	logp.Info("countbeat is running! Hit CTRL-C to stop it.")
 
-	var err error
-	bt.client, err = b.Publisher.Connect()
-	if err != nil {
-		return err
-	}
-
+	bt.client = b.Publisher.Connect()
 	ticker := time.NewTicker(bt.config.Period)
 	counter := 1
 	for {
-		select {
-		case <-bt.done:
-			return nil
-		case <-ticker.C:
-		}
+			select {
+			case <-bt.done:
+					return nil
+			case <-ticker.C:
+			}
 
-		event := beat.Event{
-			Timestamp: time.Now(),
-			Fields: common.MapStr{
-				"type":    b.Info.Name,
-				"counter": counter,
-			},
-		}
-		bt.client.Publish(event)
-		logp.Info("Event sent")
-		counter++
+			event := common.MapStr{ 
+					"@timestamp": common.Time(time.Now()), 
+					"type":       b.Name,
+					"counter":    counter,
+			}
+			bt.client.PublishEvent(event) 
+			logp.Info("Event sent")
+			counter++
 	}
-}
+}}
 
 // Stop stops countbeat.
 func (bt *Countbeat) Stop() {
--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import (
 	"os"
 
 	"github.com/HariomSaini/countbeat/cmd"
+	"github.com/elastic/beats/filebeat/beater"
+	"github.com/elastic/beats/libbeat/cmd/instance"
 
 	_ "github.com/HariomSaini/countbeat/include"
 )
+
+var RootCmd = cmd.GenRootCmdWithSettings(beater.New, instance.Settings{Name: "countbeat"})
 
 func main() {
 	if err := cmd.RootCmd.Execute(); err != nil {
